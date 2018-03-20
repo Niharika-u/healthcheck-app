33a2701a2c1a782d13a4379c71dd468e64296053
Shubhankar.Raj <Shubhankar.Raj@go-mmt.com>
2018-03-20 14:35:44 +0530

    Removing the frontend of Repository

--- src/healthcheckfrontend/src/app/app-services/util.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class UtilService {
-
-  envTypes: Array<string> = ["QA", "STAGING", "PRODUCTION"];
-  constructor() { }
-
-}
--- src/healthcheckfrontend/src/app/project.ts
+++
@@ -1,7 +0,0 @@
-
-export class Project {
-  projectId: string;
-  projectName: string;
-  //createdBy: string;
-  projectCreatedAt: Date;
-}
